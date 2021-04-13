f8eb7107fe7fe698202ea5b8d2a255cef1dad3c0
vangie <wjidian@gmail.com>
2021-04-13 19:52:59 +0800

    init

---
+++ src/main.js
@@ -0,0 +1,76 @@
+let html = document.querySelector("#html");
+let style=document.querySelector('#style');
+let string=`/*你好，我是一名前端新人
+接下来我要加样式了
+首先 准备一个div*/
+#div1{
+    border:1px solid red;
+    width:200px;
+    height:200px;
+}
+/*接下来我把div变成一个八卦图
+请看，把div变成一个圆
+*/
+#div1{
+    border-radius:50%;
+    box-shadow:0 0 3px rgba(0,0,0,0.5);
+    border:none;
+}
+/*八卦分阴阳
+白色为阳 黑为阴
+*/
+#div1{
+    background: linear-gradient(90deg, rgba(255,255,255,1) 0%, rgba(255,255,255,1) 50%, rgba(0,0,0,1) 50%, rgba(0,0,0,1) 100%);
+}
+/*加风火轮*/
+#div1::before{
+    width:100px;
+    height:100px;
+    top:0;
+    left:50%;
+    transform:translateX(-50%);
+    background:black;
+    border-radius:50%;
+    background: radial-gradient(circle, rgba(255,255,255,1) 0%, rgba(255,255,255,1) 25%, rgba(0,0,0,1) 25%, rgba(0,0,0,1) 100%);
+}
+#div1::after{
+    width:100px;
+    height:100px;
+    bottom:0;
+    left:50%;
+    transform:translateX(-50%);
+    background:white;
+    border-radius:50%;
+    background: radial-gradient(circle, rgba(0,0,0,1) 0%, rgba(0,0,0,1) 25%, rgba(255,255,255,1) 25%, rgba(255,255,255,1) 100%, rgba(0,0,0,1) 100%);
+}
+
+
+`;
+let string2='';
+
+let n=0;
+
+let step = () => {
+    setTimeout(() => {
+        if (string[n]==='\n'){
+            string2+='<br>';
+        }else if(string[n]===' '){
+            string2+='&nbsp';
+        }else{
+            string2+=string[n];
+        }
+    
+        html.innerHTML=string2;
+        style.innerHTML=string.substring(0,n)
+        window.scrollTo(0, 99999);
+        html.scrollTo(0, 99999);
+        n = n + 1;
+        if(n<string.length){
+            step();
+        }else{
+        }
+    },50);
+};
+step();
+
+
